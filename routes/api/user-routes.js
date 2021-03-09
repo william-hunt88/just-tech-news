bea03d29e6dfd42f41a440c988d1d539814e3599
w.hunt <wfh2d88@gmail.com>
2021-03-09 12:26:32 -0600

    13.2. done

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -94,4 +94,26 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
+});
+
 module.exports = router;
